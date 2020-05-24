4364e67bea10bf772c125fe870357f419762505b
pilferst <alexey.a.leontiev@gmail.com>
2020-05-24 16:39:08 +0300

    the next vesrion, repaired  some bugs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""This module get backups form routers. There are two type of backups. The frist one is a text file. The second one is binary file. If the module see in a dictorinary more than 10 files, the module delete reast of them. """
 import paramiko
 import os
 import yaml
@@ -5,21 +6,28 @@ import time
 import datetime
 from  concurrent.futures import ThreadPoolExecutor
 import scp
+from typing import Tuple
+from typing import Dict
+from typing import List
+from typing import Any
 
+def dir_check(hostname: str) -> None:
+    link : str
 
-
-
-def dir_check(hostname):
     link="/home/python/scripts/production_backup/backup_files/"
     if not os.path.exists(link + hostname):
         os.makedirs(link + hostname)
-
     return None
 
 
-def export_device(router):
-#    print("export", router[0], router[1])
-#    print("export", router[0], router[1], credentionals["username"], credentionals["password"],credentionals["port"])
+def export_device(router: Tuple[str,str] ) -> None:
+    link : str
+    current_date : str
+    command : str
+    fail : str
+    global credentionals
+
+
     link="/home/python/scripts/production_backup/backup_files/"
     current_date=datetime.datetime.today().strftime('%d-%m-%Y')
     command='/export'
@@ -37,12 +45,17 @@ def export_device(router):
 
     except:
         fail=current_date+'-backup-export-fail'
-        with open(link + device[0] + '/' + fail, "w") as file_write:
+        with open(link + router[0] + '/' + fail, "w") as file_write:
             file_write.write(fail)
         return None
 
-def backup_device(router):
-#    print("backup", router[0], router[1])
+def backup_device(router: Tuple[str,str]) -> None:
+    link : str
+    current_date : str
+    command : str
+    fail : str
+    global credentionals
+
     link="/home/python/scripts/production_backup/backup_files/"
     current_date=datetime.datetime.today().strftime('%d-%m-%Y')
     command='/system backup save dont-encrypt=yes name=Daily-'+current_date
@@ -60,32 +73,32 @@ def backup_device(router):
         time.sleep(3)
         stdin,stdout,stderr = client.exec_command(command_remove)
         client.close()
-        
 
     except:
         fail=current_date+'-backup-fail'
-        with open(link + device[0] + '/' + fail, "w") as file_write:
+        with open(link + router[0] + '/' + fail, "w") as file_write:
             file_write.write(fail)
     return None
 
-def concurrent_export_device(routers, limit):
-#    print("concurrent_export_device",credentionals,list(routers))
+def concurrent_export_device(routers : List[Tuple[str,str]], limit : int ) -> None:
     with ThreadPoolExecutor(max_workers=limit) as executor:
         f_result = executor.map(export_device, routers)
-
     return None
 
 
-def concurrent_backup_device(routers, limit):
-#    print("concurrent_backup_device")
+def concurrent_backup_device(routers : List[Tuple[str,str]], limit : int) -> None :
     with ThreadPoolExecutor(max_workers=limit) as executor:
         backup_result = executor.map(backup_device, routers)
-
     return None
 
-def delete_old_files(router):
+def delete_old_files(router: str ) -> None:
+    link : str
+    max_files : int
+    list_export: List[Tuple[str,float]]
+    list_backup: List[Tuple[str,float]]
+
+
     link="/home/python/scripts/production_backup/backup_files/"+router+"/"
-#    print(link)
     max_files=10
     list_export=[]
     list_backup=[]
@@ -93,8 +106,10 @@ def delete_old_files(router):
     for id in os.listdir(link):
         if ".export" in id:
             list_export.append((id,os.stat(link+id).st_mtime))
+            os.chmod(link + id, 0o444)
         elif ".backup" in id:
             list_backup.append((id,os.stat(link+id).st_mtime))
+            os.chmod(link + id, 0o444)
     list_export=sorted(list_export, key = lambda x : x[1])
     list_backup=sorted(list_backup, key = lambda x : x[1])
 
@@ -107,24 +122,26 @@ def delete_old_files(router):
         del_list_backup=list_backup[:len(list_backup)-max_files]
         for id_del in del_list_backup:
             os.remove(link + id_del[0])
-
-
+    return None
 
 
 if __name__ == "__main__":
+    limit : int
+    credentionals : Dict[str,str]
+    routers : List[Tuple[str,str]]
+
     limit=32
-    credentionals={}
+    credentionals={} 
     current_date=datetime.datetime.today().strftime('%d-%m-%Y')
 
-    with open ("credentionals.yml","r") as file_read:
+    with open ("/home/python/scripts/production_backup/credentionals.yml","r") as file_read:
         temp_credentionals=yaml.safe_load(file_read)
     for id in temp_credentionals:
         credentionals.update(id)
-#    print(credentionals)
 
-    with open ("routers.yml","r") as file_read:
-        routers=yaml.safe_load(file_read)
-    routers=list(routers.items())
+    with open ("/home/python/scripts/production_backup/routers.yml","r") as file_read:
+        routers_dict=yaml.safe_load(file_read)
+    routers=list(routers_dict.items())
 
     for id in routers:
         dir_check(id[0])
@@ -137,9 +154,3 @@ if __name__ == "__main__":
             delete_old_files(id[0])
     except:
         pass
-
-
-
-
-
-
